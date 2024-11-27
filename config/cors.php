1cf55dd9d0939c1d99f57aa3c8a213607a9363dd
muslih24 <muslih259@gmail.com>
2024-11-27 23:25:51 +0700

    cors

--- config/cors.php
+++ config/cors.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie', 'css/*', 'js/*', 'images/*'],
 
     'allowed_methods' => ['*'],
 
